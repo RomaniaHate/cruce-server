2c84f60ba866293688e0d189f6ec940aa01404c8
Miru5 <miru_katzy@yahoo.com>
2016-08-16 22:03:06 +0300

    mongo

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = 'mongodb://root:toor@ds161175.mlab.com:61175/heroku_1tq03tlx';
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Error:', err);
+  } else {
+    console.log('Connection established.', url);
+    db.close();
+  }
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port '+ port);
 });
